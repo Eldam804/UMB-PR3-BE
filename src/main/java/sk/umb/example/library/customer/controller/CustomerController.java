aad5b27e8b57b1255c309a9511578d9e85267c9b
Eldam804 <adam.kleinedler@gmail.com>
2023-02-13 17:19:08 +0100

    Added customer calls

--- src/main/java/sk/umb/example/library/customer/controller/CustomerController.java
+++ src/main/java/sk/umb/example/library/customer/controller/CustomerController.java
@@ -1,8 +1,6 @@
 package sk.umb.example.library.customer.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,5 +11,22 @@ public class CustomerController {
     @GetMapping("/api/customers")
     public List<Object> searchCustomers(@RequestParam(required = false) String lastName){
         return Collections.emptyList();
+    }
+    @GetMapping("/api/customers/{customerId}")
+    public void getCustomer(@PathVariable Long customerId){
+        System.out.println("get customer called ID:" + customerId);
+    }
+    @PostMapping("/api/customers")
+    public void createCustomer(){
+        System.out.println("Created customer");
+    }
+
+    @PutMapping("/api/customers/{customerId}")
+    public void updateCustomer(@PathVariable Long customerId){
+        System.out.println("Update customer at:" + customerId);
+    }
+    @DeleteMapping("/api/customers/{customerId}")
+    public void deleteCustomer(@PathVariable Long customerId){
+        System.out.println("Customer with ID:" + customerId);
     }
 }
