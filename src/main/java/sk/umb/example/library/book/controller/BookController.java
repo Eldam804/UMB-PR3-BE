d02e1b08f490293cc5b48f0fb998d56b7a866c23
Eldam804 <adam.kleinedler@gmail.com>
2023-02-18 20:46:26 +0100

    Added CategoryController, and renamed all methods to fit the assigned task

--- src/main/java/sk/umb/example/library/book/controller/BookController.java
+++ src/main/java/sk/umb/example/library/book/controller/BookController.java
@@ -8,23 +8,23 @@ import java.util.List;
 @RestController
 public class BookController {
     @GetMapping("/api/books")
-    public List<Object> searchBooks(@RequestParam(required = false) String name){
+    public List<Object> listResource(@RequestParam(required = false) String name){
         return Collections.emptyList();
     }
     @GetMapping("/api/books/{id}")
-    public void getBook(@PathVariable long id){
+    public void retrieveDetail(@PathVariable long id){
         System.out.println("Printing book at id:"+id);
     }
     @PostMapping("/api/books")
-    public void addBook(){
+    public void createResource(){
         System.out.println("Added book");
     }
     @PutMapping("/api/books/{bookId}")
-    public void updateBook(@PathVariable long bookId){
+    public void updateResource(@PathVariable long bookId){
         System.out.println("Updated book at:" + bookId);
     }
     @DeleteMapping("/api/books/{bookId}")
-    public void deleteBook(@PathVariable long bookId){
+    public void deleteResource(@PathVariable long bookId){
         System.out.println("Deleted book: " + bookId);
     }
 }
