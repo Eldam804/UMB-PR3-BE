4e9a4cbdb2b94080a2bb70d195ef42d9096b3972
Eldam804 <adam.kleinedler@gmail.com>
2023-02-20 17:35:01 +0100

    Added service for customer

--- src/main/java/sk/umb/example/library/customer/controller/CustomerController.java
+++ src/main/java/sk/umb/example/library/customer/controller/CustomerController.java
@@ -1,33 +1,46 @@
 package sk.umb.example.library.customer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sk.umb.example.library.customer.service.CustomerDto;
+import sk.umb.example.library.customer.service.CustomerRequestDto;
+import sk.umb.example.library.customer.service.CustomerService;
 
-        import java.util.Collections;
+import java.util.Collections;
         import java.util.List;
 
 @RestController
 public class CustomerController {
+    private final CustomerService customerService;
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
 
     @GetMapping("/api/customer")
-    public void searchCustomers(@RequestParam(required = false) String lastName){
+    public List<CustomerDto> getCustomers(@RequestParam(required = false) String lastName){
         System.out.println("Search Customer called.");
-//        return Collections.emptyList();
+        return customerService.getAllCustomers();
+        //return Collections.emptyList();
     }
 
-    @PostMapping("/api/customer/{customerID}")
-    public void getCustomers(@PathVariable Long customerID) {
-        System.out.println("Get customer called: " + customerID);
+    @PostMapping("/api/customer")
+    public Long createCustomers(@RequestBody CustomerRequestDto customerId) {
+        return customerService.createNewCustomer(customerId);
     }
-    @GetMapping("/api/customers/{customerId}")
-    public void getCustomer(@PathVariable Long customerId){
+    @GetMapping("/api/customer/{customerId}")
+    public CustomerDto getCustomer(@PathVariable Long customerId){
         System.out.println("get customer called ID:" + customerId);
+        return customerService.getCustomerById(customerId);
     }
-    @PutMapping("/api/customer/{customerID}")
-    public void updateCustomer(@PathVariable Long customerID){
-        System.out.println("update customer called: id = " + customerID);
+    @PutMapping("/api/customer/{customerId}")
+    public void updateCustomer(@PathVariable Long customerId, @RequestBody CustomerRequestDto customer){
+        System.out.println("update customer called: id = " + customerId);
+
+        customerService.updateCustomer(customerId, customer);
     }
 
-    @DeleteMapping("/api/customers/{customerId}")
+    @DeleteMapping("/api/customer/{customerId}")
     public void deleteCustomer(@PathVariable Long customerId){
         System.out.println("Customer with ID:" + customerId);
     }
