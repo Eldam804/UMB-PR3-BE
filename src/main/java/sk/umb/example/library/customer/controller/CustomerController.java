75e85eb484c83a7723ebd60529c903948702010e
Eldam804 <adam.kleinedler@gmail.com>
2023-03-12 18:18:59 +0100

    Renamed customer api calls

--- src/main/java/sk/umb/example/library/customer/controller/CustomerController.java
+++ src/main/java/sk/umb/example/library/customer/controller/CustomerController.java
@@ -17,30 +17,30 @@ public class CustomerController {
     }
 
 
-    @GetMapping("/api/customers")
+    @GetMapping("/api/customer")
     public List<CustomerDto> getCustomers(@RequestParam(required = false) String lastName){
         System.out.println("Search Customer called.");
         return customerService.getAllCustomers();
         //return Collections.emptyList();
     }
 
-    @PostMapping("/api/customers")
+    @PostMapping("/api/customer")
     public Long createCustomers(@RequestBody CustomerRequestDto customerId) {
         return customerService.createNewCustomer(customerId);
     }
-    @GetMapping("/api/customers/{customerId}")
+    @GetMapping("/api/customer/{customerId}")
     public CustomerDto getCustomer(@PathVariable Long customerId){
         System.out.println("get customer called ID:" + customerId);
         return customerService.getCustomerById(customerId);
     }
-    @PutMapping("/api/customers/{customerId}")
+    @PutMapping("/api/customer/{customerId}")
     public void updateCustomer(@PathVariable Long customerId, @RequestBody CustomerRequestDto customer){
         System.out.println("update customer called: id = " + customerId);
 
         customerService.updateCustomer(customerId, customer);
     }
 
-    @DeleteMapping("/api/customers/{customerId}")
+    @DeleteMapping("/api/customer/{customerId}")
     public void deleteCustomer(@PathVariable Long customerId){
         customerService.deleteCustomer(customerId);
     }
