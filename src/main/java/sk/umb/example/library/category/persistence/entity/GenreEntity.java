bcb2beca583ee95319c0091a85bba209f89789c7
Tomas306 <124598342+Tomas306@users.noreply.github.com>
2023-03-19 22:12:54 +0100

    fixed2

--- src/main/java/sk/umb/example/library/category/persistence/entity/GenreEntity.java
+++ src/main/java/sk/umb/example/library/category/persistence/entity/GenreEntity.java
@@ -8,14 +8,14 @@ import javax.persistence.GeneratedValue;
 public class GenreEntity {
     @Id
     @GeneratedValue
-    private long id;
+    private Long id;
     private String name;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
