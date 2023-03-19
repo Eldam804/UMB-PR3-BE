0ffef9c8f828fbf70e9bd62ef11ab4641086a191
Eldam804 <adam.kleinedler@gmail.com>
2023-03-19 22:15:05 +0100

    renamed import

--- src/main/java/sk/umb/example/library/category/persistence/entity/GenreEntity.java
+++ src/main/java/sk/umb/example/library/category/persistence/entity/GenreEntity.java
@@ -1,9 +1,9 @@
 package sk.umb.example.library.category.persistence.entity;
 
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Entity
 public class GenreEntity {
     @Id
